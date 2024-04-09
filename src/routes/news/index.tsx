d720fe35f731bf952072d27e74cfe20fbb7bf6e9
william <test@test.com>
2024-04-09 11:16:59 -0500

    add /login & card component

--- src/routes/news/index.tsx
+++ src/routes/news/index.tsx
@@ -4,10 +4,8 @@ import { NEWS } from '~/utils/newsArray';
 
 export default component$(() => {
   return (
-    <div class="w-full py-10">
-      {NEWS.map(newItem=>(
-          <CardNew  key={newItem.id} title={newItem.name} description={newItem.description} id={newItem.id} date={newItem.date}/>
-      ))}
-    </div>
+    <div>
+      <Card ImgUrl='' title="test" description="This is a test"/>
+    </div> 
   );
 });
