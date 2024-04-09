4f2de4efa6c0319f7e6c11feaee21385e0595cf4
isabelatapias1 <77413685+isabelatapias1@users.noreply.github.com>
2024-04-09 10:46:29 -0500

    update font

--- src/components/common/header/header.tsx
+++ src/components/common/header/header.tsx
@@ -3,6 +3,7 @@ import { NavLink } from "../navLink/navLink";
 import ImgParamoLogo from '../../../../public/images/paramo_logo.png?jsx';
 import { SideBarMenu } from "../sideBarMenu/sideBarMenu";
 
+
 interface HeaderProps {
 
 }
@@ -26,11 +27,11 @@ export default component$<HeaderProps>(() => {
         <nav class="w-full border-b-2 p-4 border-b-black dark:border-b-white dark:text-white">
           <div class="flex flex-wrap items-stretch justify-stretch">
             <div class="p-2 border-r-2 border-r-black dark:border-r-white">
-              <button type="button" class="flex items-center" data-drawer-target="drawer-navigation" data-drawer-show="drawer-navigation" aria-controls="drawer-navigation">
+              <button type="button" class="flex items-center font-bold" data-drawer-target="drawer-navigation" data-drawer-show="drawer-navigation" aria-controls="drawer-navigation">
                 <span class="material-symbols-outlined">
                   menu
                 </span>
-                Menu
+                 MENU
               </button>
             </div>
             <div class="flex-auto p-2 text-center hidden md:block md:border-r-2 md:border-r-black dark:md:border-r-white">
@@ -65,18 +66,17 @@ export default component$<HeaderProps>(() => {
             </div>
           </div>
         </nav>
-        <ImgParamoLogo class="w-1/3 mx-auto" alt="Paramo Logo" />
-        {/* menu navigato */}
-        <div class="flex justify-evenly text-center text-gray-500 sm:overflow-x-scroll dark:text-gray-400 p-4 w-5/6 mx-auto border-y-2 border-y-black dark:border-y-white">
-          <NavLink href="/news" activeClass="text-blue-600 bg-gray-100 active dark:bg-gray-800 dark:text-blue-500" class="inline-block p-4 hover:text-gray-600 hover:bg-gray-50 dark:hover:bg-gray-800 dark:hover:text-gray-300">Noticias</NavLink>
+        <ImgParamoLogo class="w-1/3 mx-auto" alt="Paramo Logo"/>
+        <div class=" font-bold flex justify-evenly text-center text-black-500 dark:text-gray-400 p-4 w-5/6 mx-auto border-y-2 border-y-black dark:border-y-white">
+          <NavLink href="/news" activeClass="text-black-600 bg-white-100 active dark:bg-gray-800 dark:text-blue-500" class="inline-block p-4 hover:text-red-600 hover:bg-gray-50 dark:hover:bg-gray-800 dark:hover:text-gray-300">NOTICIAS</NavLink>
           <span class="border-x border-x-black dark:border-x-white"/>
-          <NavLink href="/ideas" activeClass="text-blue-600 bg-gray-100 active dark:bg-gray-800 dark:text-blue-500" class="inline-block p-4 hover:text-gray-600 hover:bg-gray-50 dark:hover:bg-gray-800 dark:hover:text-gray-300">Ideas</NavLink>
+          <NavLink href="/ideas" activeClass="text-blue-600 bg-gray-100 active dark:bg-gray-800 dark:text-blue-500" class="inline-block p-4 hover:text-red-600 hover:bg-gray-50 dark:hover:bg-gray-800 dark:hover:text-gray-300">IDEAS</NavLink>
           <span class="border-x border-x-black dark:border-x-white"/>
-          <NavLink  href="/fan-insights" activeClass="text-blue-600 bg-gray-100 active dark:bg-gray-800 dark:text-blue-500" class="inline-block p-4 hover:text-gray-600 hover:bg-gray-50 dark:hover:bg-gray-800 dark:hover:text-gray-300">Fans</NavLink>
+          <NavLink  href="/fan-insights" activeClass="text-blue-600 bg-gray-100 active dark:bg-gray-800 dark:text-blue-500" class="inline-block p-4 hover:text-red-600 hover:bg-gray-50 dark:hover:bg-gray-800 dark:hover:text-gray-300">FANS</NavLink>
           <span class="border-x border-x-black dark:border-x-white"/>
-          <NavLink href="/backstage-with-brands" activeClass="text-blue-600 bg-gray-100 active dark:bg-gray-800 dark:text-blue-500" class="inline-block p-4 hover:text-gray-600 hover:bg-gray-50 dark:hover:bg-gray-800 dark:hover:text-gray-300">Marcas</NavLink>
+          <NavLink href="/backstage-with-brands" activeClass="text-blue-600 bg-gray-100 active dark:bg-gray-800 dark:text-blue-500" class="inline-block p-4 hover:text-red-600 hover:bg-gray-50 dark:hover:bg-gray-800 dark:hover:text-gray-300">MARCAS</NavLink>
           <span class="border-x border-x-black dark:border-x-white"/>
-          <NavLink href="/" activeClass="" class="inline-block p-4 hover:text-gray-600 hover:bg-gray-50 dark:hover:bg-gray-800 dark:hover:text-gray-300">Buscar</NavLink>
+          <NavLink href="/" activeClass="" class="inline-block p-4 hover:text-red-600 hover:bg-gray-50 dark:hover:bg-red-800 dark:hover:text-red-300">BUSCAR</NavLink>
         </div>
       </div>
     </div>
