636a0db0bf8df2083cfa915413626430adbb8aaa
Diego <diegoarojas@hotmail.es>
2024-04-17 13:37:50 -0500

    fix footer image

--- src/components/common/footer/footer.tsx
+++ src/components/common/footer/footer.tsx
@@ -1,6 +1,6 @@
 import { component$ } from "@builder.io/qwik";
 import { useServerTimeLoader } from "../../../routes/layout";
-import ImgParamoLogo from "/images/paramo_logo.png";
+import Ima from "../../../../public/images/paramo_logo.png";
 
 export interface FooterProps {
   class?: string;
@@ -12,13 +12,8 @@ export default component$<FooterProps>((props) => {
   return (
     <footer class={props.class+"  border border-red-500"}>
       <div class="flex items-center 	 pt-4 mx-auto text-center w-4/5 dark:text-white border-t-2 border-t-black dark:border-t-white">
-      <img
-                src={ImgParamoLogo}
-                width="120"
-                height="140"
-                
-              />
-        <span class="">{serverTime.value.date}</span>
+        <img src={Ima} width={100} height={100} alt=""/>
+        <p class="">{serverTime.value.date}</p>
       </div>
     </footer>
   );
