1ac8988d8d818aaf99c765559e76cff996850ce1
william <test@test.com>
2024-04-17 19:13:38 -0500

    Delete unused validation

--- src/components/form/TextInput/text-input.tsx
+++ src/components/form/TextInput/text-input.tsx
@@ -25,7 +25,7 @@ export const TextInput = component$(
           <label for={props.name} class="block mb-2 text-sm font-medium text-red-700 dark:text-red-500">
             {props.label} {props.required && <span class="text-red-500">*</span>}
           </label>
-          <input type={props.type} id={props.name} class={"bg-red-50 border border-red-500 text-red-900 placeholder-red-700 text-sm rounded-lg focus:ring-red-500 dark:bg-gray-700 focus:border-red-500 block w-full p-2.5 dark:text-red-500 dark:placeholder-red-500 dark:border-red-500" + props.class ?? ""} placeholder={props.placeholder} />
+          <input type={props.type} id={props.name} class={"bg-red-50 border border-red-500 text-red-900 placeholder-red-700 text-sm rounded-lg focus:ring-red-500 dark:bg-gray-700 focus:border-red-500 block w-full p-2.5 dark:text-red-500 dark:placeholder-red-500 dark:border-red-500" + props.class} placeholder={props.placeholder} />
           <p class="mt-2 text-sm text-red-600 dark:text-red-500">{props.error}</p>
         </>) : (<>
           <label for={props.name} class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">
@@ -35,7 +35,7 @@ export const TextInput = component$(
             <div class="absolute inset-y-0 start-0 flex items-center ps-3.5 pointer-events-none">
               <Slot />
             </div>
-            <input type={props.type} id={props.name} class={"bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full ps-10 p-2.5  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" + props.class ?? ""} placeholder={props.placeholder} />
+            <input type={props.type} id={props.name} class={"bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full ps-10 p-2.5  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" + props.class} placeholder={props.placeholder} />
           </div>
         </>)}
       </div>
