c00ffe28a5bba8eeee545632ad56371f79d3519c
william <test@test.com>
2024-04-18 18:24:51 -0500

    delete "back" uninstall prisma

--- src/routes/login/index.tsx
+++ src/routes/login/index.tsx
@@ -1,14 +1,9 @@
-import { component$, useSignal } from '@builder.io/qwik';
-import { Form, routeAction$, routeLoader$, zod$ } from '@builder.io/qwik-city';
+import { component$ } from '@builder.io/qwik';
+import { Form, routeAction$, zod$ } from '@builder.io/qwik-city';
 import { loginRequestSchema } from '~/schemas/login.schema';
-import { createJWT } from '~/utils/helpers';
-import { login } from '~/services/AuthAppService';
-import { UserResponse } from '~/models/Response/login.model';
-import { log } from '~/services/LogginService';
 import { TextInput } from '~/components/form/TextInput/text-input';
 
 export const useLogin = routeAction$(async (data, requestEvent) => {
-  /*
   const response = await fetch("http://localhost:8080/login", {
     method: "POST",
     body: JSON.stringify(data),
@@ -16,32 +11,7 @@ export const useLogin = routeAction$(async (data, requestEvent) => {
   const body = await response.json();
   console.log(body);
   requestEvent.headers.set("Authorization", `Bearer ${body.message}`);;
-  */
-
-  const response = await login(data);
-  if(!response.success){
-    return requestEvent.fail(response.status, response.error);
-  }
-
-  const user : UserResponse = {
-    id: response.data.id,
-    email: response.data.email,
-    name: response.data.name,
-    created_at: response.data.created_at
-  }
-  const jwt = await createJWT(user);
-
-  console.log("\n\nLOGIN:\n");
-  log(`User ${user.email} logged in: ${jwt}`);
-  //requestEvent.headers.set("Authorization", `Bearer ${jwt}`);;
-  requestEvent.cookie.set("access_token", jwt, {
-    httpOnly: true,
-    secure: true,
-  });
-  console.log("\n\nLOGIN:\n");
-  requestEvent.json(200, {
-    access_token: jwt,
-  });
+  return body;
 },
   zod$(loginRequestSchema)
 );
@@ -50,7 +20,6 @@ export default component$(() => {
   const loginAction = useLogin();
   const email = loginAction.formData?.get("email");
   const password = loginAction.formData?.get("password");
-  //console.log(loginAction.value);
 
   return (
     <Form action={loginAction} class="max-w-sm mx-auto my-10">
