24d7d158022972e573711552f5eaeac604b83423
william <test@test.com>
2024-04-20 13:05:56 -0500

    Add back & front folders

---
+++ back/services/AuthService.cs
@@ -0,0 +1,17 @@
+namespace paramo.services;
+
+/*
+static async Task<IResult> Login(string email, string password)
+{
+  if(email == "admin@test.com")
+  {
+    return Results.Ok(new { token = "token-secret" });
+  }
+  return Results.Unauthorized();
+}
+
+static async Task<IResult> Logout()
+{
+  //TODO: Use memory to keep track of active tokens, remove it when logout or the expires?
+}
+*/
---
+++ back/services/UserService.cs
@@ -0,0 +1,12 @@
+namespace paramo.services;
+
+/*
+static async Task<IResult> GetByEmail(string email)
+{
+  if(email == "admin@test.com")
+  {
+    return Results.Ok(new { token = "token-secret" });
+  }
+  return Results.Unauthorized();
+}
+*/
