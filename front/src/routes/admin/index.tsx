5f16ef89f8195b94eb4e4d668c039de3ddc54a28
william <test@test.com>
2024-04-21 13:17:02 -0500

    Add middleware for admin route

--- front/src/routes/admin/index.tsx
+++ front/src/routes/admin/index.tsx
@@ -1,5 +1,16 @@
 import { component$, useStore } from "@builder.io/qwik";
-import { Link } from "@builder.io/qwik-city";
+import { Link, RequestHandler } from "@builder.io/qwik-city";
+import { supabase } from "~/utils/supabase";
+
+
+export const onRequest: RequestHandler = async requestEvent => {
+  const session = await supabase.auth.getSession();
+  console.log("\n\nADMIN:\n", session);
+  if(session.error || session.data.session == null){
+    requestEvent.status(401);
+    throw requestEvent.redirect(302, "/");
+  }
+}
 
 export default component$(() => {
   const states = useStore({
