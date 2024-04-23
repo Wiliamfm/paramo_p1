3ae3c8a3ac1529cc7f4f64a2fa8f9e48034b025b
william <test@test.com>
2024-04-23 03:35:06 -0500

    add news models & schemas

---
+++ src/models/news.ts
@@ -0,0 +1,28 @@
+export enum NewsType {
+  title = "title",
+  subtitle = "subtitle",
+  content = "content",
+  bannerImage = "bannerImage",
+  images = "images",
+  videos = "video",
+}
+
+export enum NewsInputType {
+  title = "title",
+  subtitle = "subtitle",
+  description = "description",
+  banner = "banner",
+  carousel = "carousel",
+  video = "video",
+}
+
+export interface NewsComponent {
+  type: NewsType;
+  value: string | string[];
+  order: number;
+}
+
+export interface News{
+  title: string;
+  components: NewsComponent[];
+}
---
+++ src/schemas/news.schema.ts
@@ -0,0 +1,14 @@
+import { z } from '@builder.io/qwik-city';
+import { NewsInputType } from '~/models/news';
+
+export const newsInputSchema = {
+  components: z.array(z.object({
+    title: z.string().min(3, "The title must have 3 characters or more.").optional(),
+    type: z.nativeEnum(NewsInputType),
+    subtitle: z.string().optional(),
+    content: z.string().min(10, "The content must have 10 characters or more.").optional(),
+    bannerImage: z.string().optional(),
+    images: z.string().optional(),
+    videos: z.string().optional(),
+  })),
+}
